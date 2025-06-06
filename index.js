7b249bdd33279c3c7c329be1303ee5f2e1ef5709
viniciusIFRN <131037198+viniciusIFRN@users.noreply.github.com>
2025-06-06 11:20:13 -0300

    logo if

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 function funcao() {
     const menu = document.getElementById("menu");
+    const span = document.getElementById("spam");
     const barra = document.getElementsByClassName("barra-lateral")[0];
     
     if (menu.style.display === "block") {
       menu.style.display = "none";
       barra.style.backgroundColor = "white";
+      span.style.display = "none"
     } else {
       menu.style.display = "block";
       barra.style.backgroundColor = "#3462b0";
+      span.style.display = "block"
     }
+  }
+
+  function recarregar(){
+     location.reload()
   }
